1dd0ea5e02bff8ecaab667da9e09206dbf656e19
Shreegouri J Jahagirdar <shreegaurij@gmail.com>
2026-06-12 01:50:21 +0530

    Create 345_Reverse_Vowels_of_a_String.cpp

---
+++ Strings/345_Reverse_Vowels_of_a_String.cpp
@@ -0,0 +1,28 @@
+//Using two pointers from both ends to swap vowels while skipping consonants.
+class Solution {
+public:
+    bool isvowel(char c)
+    {
+        c=tolower(c);
+        return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
+
+    }
+    string reverseVowels(string s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right)
+        {
+            while(left<right && !isvowel(s[left]))
+            left++;
+            while(left<right && !isvowel(s[right]))
+            right--;
+            swap(s[left],s[right]);
+
+            left++;
+            right--;
+
+        }
+        return s;
+        
+    }
+};
